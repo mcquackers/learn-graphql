63d0fcfec008e4b661cd9732aeac0569665d6283
Brian McQueen <brian.d.mcqueen@gmail.com>
2016-12-11 16:06:51 -0500

    Returns custom type

--- learn.go
+++ learn.go
@@ -13,27 +13,44 @@ import (
 
 var db *sql.DB
 
+type Movie struct {
+	Uid  int    `json:"uid"`
+	Name string `json:"name"`
+}
+
+var movieType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Movie",
+	Fields: graphql.Fields{
+		"uid": &graphql.Field{
+			Type: graphql.Int,
+		},
+		"name": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
+
 var queryType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Query",
 	Fields: graphql.Fields{
 		"latestPost": &graphql.Field{
-			Type: graphql.String,
+			Type: movieType,
 			//Always has to have graphql.ResolveParams and return interface{} and
 			//error.  Ugh, out of date tutorials stink
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				stmt, err := db.Prepare("SELECT name FROM movies WHERE uid = ?")
+				stmt, err := db.Prepare("SELECT * FROM movies WHERE uid = ?")
 				if err != nil {
 					log.Fatal(err)
 				}
 				defer stmt.Close()
 
-				var name string
-				err = stmt.QueryRow("1").Scan(&name)
+				var movie Movie
+				err = stmt.QueryRow("1").Scan(&movie.Uid, &movie.Name)
 				if err != nil {
 					log.Fatal(err)
 				}
 
-				return name, nil
+				return movie, nil
 			},
 		},
 		"randInt": &graphql.Field{
@@ -42,26 +59,6 @@ var queryType = graphql.NewObject(graphql.ObjectConfig{
 				return rand.Intn(50), nil
 			},
 		},
-		// "otherInts": &graphql.Fields{
-		// 	"int1": &graphql.Field{
-		// 		Type: graphql.Int,
-		// 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-		// 			return rand.Intn(25), nil
-		// 		},
-		// 	},
-		// 	"int2": &graphql.Field{
-		// 		Type: graphql.Int,
-		// 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-		// 			return rand.Intn(10), nil
-		// 		},
-		// 	},
-		// 	"int3": &graphql.Field{
-		// 		Type: graphql.Int,
-		// 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-		// 			return rand.Intn(5000), nil
-		// 		},
-		// 	},
-		// },
 	},
 })
 
